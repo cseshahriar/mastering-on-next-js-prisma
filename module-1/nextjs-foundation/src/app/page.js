895a7d1078d93395a59331810ffa83666b6c2487
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-06-10 14:52:18 +0600

    working with static

---
+++ module-1/nextjs-foundation/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      Home page
+      {/** serving static file from public dir */}
+      <img src={"assets/shahriar.jpeg"} alt="img" />
+      <Image src="/assets/shahriar.jpeg" alt="img" width={500}
+        height={300} />
+    </>
+  );
+}
