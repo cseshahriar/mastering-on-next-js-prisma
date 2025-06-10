779a83b47292f2f754338d91ce21ea834a7a4826
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-06-10 17:53:51 +0600

    module css

--- module-1/nextjs-foundation/src/app/about/page.jsx
+++ module-1/nextjs-foundation/src/app/about/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import styles from  './page.module.css'
 
 const page = () => {
   return (
-    <div>About Page</div>
+    <div>
+      <h1 className={`${styles.myText} text-green`}>About Page</h1>
+    </div>
   )
 }
 
