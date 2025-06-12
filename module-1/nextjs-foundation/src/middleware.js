c5ba4caf1d9663fbed76c33b6839b287466889f6
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-06-12 19:19:22 +0600

    return response header

--- module-1/nextjs-foundation/src/middleware.js
+++ module-1/nextjs-foundation/src/middleware.js
@@ -1,8 +1,30 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(request) {
+export function middleware(request, response) {
+
     // manage request, response
+    const reqHeaders = new Headers(request.headers);
+    const token = reqHeaders.get("token")
+    if(token === '123-ABC') {
+        return NextResponse.next({
+            request: {headers: reqHeaders} // request header
+        })
+    } else {
+        const res =  NextResponse.json(
+            {msg: "Bad request"}, {status: 400}
+        )
+        res.headers.set('API-KEY', 'Example')
+        return res;
+    }
+
     console.log('================================================ middleware');
+    if(request.nextUrl.pathname.startWith("/api/products")) {
+        console.log('This is api for products');
+        return NextResponse.next()
+    } else if(request.nextUrl.pathname.startWith("/api/users"))  {
+        console.log('This is api for not users');
+        return NextResponse.next()
+    }
     return NextResponse.next()
 }
 
