27cbeb9f3d60d3ce1577492675203a04ef34c9d1
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-06-12 11:04:18 +0600

    image optimiztion

--- module-1/nextjs-foundation/src/components/WorkWithStatic.jsx
+++ module-1/nextjs-foundation/src/components/WorkWithStatic.jsx
@@ -6,8 +6,16 @@ const WorkWithStatic = () => {
   return (
     <div>
         <img src={"assets/shahriar.jpeg"} alt="img" width="200px" />
-      <Image src="/assets/shahriar.jpeg" alt="img" width={200}
-        height={200} />
+        <Image
+          src="/assets/shahriar.jpeg"
+          alt="img"
+          width={200}
+          height={200}
+          layout='responsive' // fixed
+          priority
+          placeholder='blur'
+          blurDataURL='data:image/png;base64,iVBORW0kGgo'
+         />
     </div>
   )
 }
