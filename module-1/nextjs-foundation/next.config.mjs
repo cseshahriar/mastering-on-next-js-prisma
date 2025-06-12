ab23064c5b3681a47156317306514e20db100a95
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-06-12 10:05:54 +0600

    next config

--- module-1/nextjs-foundation/next.config.mjs
+++ module-1/nextjs-foundation/next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    generateEtags: false,
+    trailingSlash: false,
+    productionBrowserSourceMaps: true,
+    httpAgentOptions: {
+        keepAlive: true,
+        timeout: 60000
+    },
+    compress: true,
+    distDir: "build",
+    reactStrictMode: true,
+    pageExtensions: ["js", "jsx"],
+    poweredByHeader: false,
     images: {
         domains: ['images.unsplash.com'], // Add any image domains you use
     },
@@ -13,16 +25,21 @@ const nextConfig = {
             {
                 source: "/product",
                 headers:[
-                    {'key': 'app-auth-key', value: '123-35345'}
+                    {'key': 'app-auth-key', value: '123-35345'},
                 ]
 
             },
             {
                 source: "/:path*",
                 headers:[
-                    {'key': 'app-auth-key-all', value: '123-35345'}
+                    {key: 'app-auth-key-all', value: '123-35345'},
+                    {key: 'X-Frame-Options', value: 'DENY'}, // iframe disallow
+                    {key: 'Content-Security-Policy', value: "default-src 'self';script-src 'self';child-src example.com; style-src 'self' example.com;font-src 'self'"}, // iframe disallow
+                    {
+                        key: 'Permission-Policy',
+                        value: "camera();battery=(self); browsing-topics=();geolocation=();microphone=('https://abc_domain.com)"
+                    }
                 ]
-
             }
         ]
     }
