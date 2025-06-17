e228ac71d9a07ea18fc45292ce572b5841ac0ee7
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-06-17 08:46:37 +0600

    nexted object create

--- module-7-orm-prisma-query/prisma-mysql/src/app/api/users/route.js
+++ module-7-orm-prisma-query/prisma-mysql/src/app/api/users/route.js
@@ -6,26 +6,40 @@ export async function POST(request) {
         const prisma = new PrismaClient();
         const payload = await request.json();
 
+        // nested object create
         let userData = await prisma.User.create({
             data: {
                 name: payload['name'],
                 email: payload['email'],
                 password: payload['password'],
                 role: payload['role'],
-                active: true
+                active: true,
+                profile: {
+                    create: {
+                        firstName: payload['firstName'],
+                        lastName: payload['lastName'],
+                        mobile: payload['mobile'],
+                        city: payload['city']
+                    }
+                },
+                post: {
+                    create: {
+                        title: payload['title'],
+                        description: payload['description']
+                    },
+                }
             }
         })
 
-        let profileData = await prisma.Profile.create({
-            data: {
-                firstName: payload['firstName'],
-                lastName: payload['lastName'],
-                mobile: payload['mobile'],
-                city: payload['city'],
-                userID: userData.id
-            }
-        })
-
+        // let profileData = await prisma.Profile.create({
+        //     data: {
+        //         firstName: payload['firstName'],
+        //         lastName: payload['lastName'],
+        //         mobile: payload['mobile'],
+        //         city: payload['city'],
+        //         userID: userData.id
+        //     }
+        // })
         return NextResponse.json({status: "success", data: userData})
     } catch (error) {
         return NextResponse.json({status: "fail", data:error})
