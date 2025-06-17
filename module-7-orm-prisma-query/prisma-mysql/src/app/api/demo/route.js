7d2c304a348b1eed1e62940ce78fd9936e12166e
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-06-17 07:55:41 +0600

    employee post api and employee create many

--- module-7-orm-prisma-query/prisma-mysql/src/app/api/demo/route.js
+++ module-7-orm-prisma-query/prisma-mysql/src/app/api/demo/route.js
@@ -8,14 +8,22 @@ export async function POST(request) {
         // let data = await prisma.Employee.create({
         //     data:payload
         // })
-        let data = await prisma.Employee.create({
-            data: {
-                name: payload['name'],
-                designation: payload['designation'],
-                city: payload['city'],
-                salary: payload['salary']
-            }
+        // let data = await prisma.Employee.create({
+        //     data: {
+        //         name: payload['name'],
+        //         designation: payload['designation'],
+        //         city: payload['city'],
+        //         salary: payload['salary']
+        //     }
+        // })
+
+        let data = await prisma.Employee.createMany({
+            data: [
+                {name: "Ayesha", designation: "Student", city: "Dhaka", salary: 0},
+                {name: "Ayesha", designation: "Student", city: "Dhaka", salary: 0}
+            ]
         })
+
         return NextResponse.json({status: "success", data: data})
     } catch (error) {
         return NextResponse.json({status: "fail", data:error})
